cf15720bf8099f3008cae395de03e53916737e6b
Walter Menendez <walterm@mit.edu>
2015-03-17 03:44:59 -0400

    Adding in results route

--- js/router.js
+++ js/router.js
@@ -1,8 +1,13 @@
 Lickr.Router.map(function() {
     this.resource('question', {path: '/q/:q_id'});
+    this.resource('results', {path: '/results'});
 });
 
 Lickr.QuestionRoute = Ember.Route.extend({
+    setupController: function(controller, model) {     
+        controller.set('model', model);
+        controller.set('numModels', 4); // TODO: how can I dynamically code this value?
+    },
     model: function (params) {
         return this.store.find('question', params.q_id);
     }
