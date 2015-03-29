df9b0ab4223c5ed00e1afc86fd3f36eda1b7ecc8
Walter Menendez <walterm@mit.edu>
2015-03-29 03:48:23 -0400

    State variable to keep track of images selected

--- js/controllers/questions_controller.js
+++ js/controllers/questions_controller.js
@@ -1,12 +1,23 @@
-Lickr.QuestionController = Ember.ObjectController.extend({
+Lickr.ApplicationController = Ember.Controller.extend({
+    selectedImages: []
+});
 
+Lickr.QuestionController = Ember.ObjectController.extend({
+    needs: ['application'],
+    selectedImages: Ember.computed.alias('controllers.application.selectedImages'),
     actions: {
         test: function() {
             var current = this.get('model').get('id');
             current = parseInt(current,10) + 1;
+
             if(current > this.get('numModels')){
+                // TODO: post to Flask to do image processing
+                // TODO: redirect to results with hex codes that way
                 this.transitionToRoute('results');
             } else this.transitionToRoute('question',current);
+        },
+        addImage: function(img) {
+            this.get('selectedImages').push(img);
         }
     }
 });
