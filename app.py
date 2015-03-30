a7501d61b1490118bdd1596a34d9a971cca0fc70
Walter Menendez <walterm@mit.edu>
2015-03-29 20:40:34 -0400

    Adding image processing route placeholder

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from cors import crossdomain
 import PIL
 app = Flask(__name__)
 
+
+@app.route("/process_imgs", methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
+def process_images():
+    print request.form.getlist('imgs[]')
+    return 'test'
+
+
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, host='0.0.0.0', debug=True)
