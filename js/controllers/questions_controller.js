1d3d58a7ca684c965a2aa888c1d069b51c772992
Walter Menendez <walterm@mit.edu>
2015-04-06 00:45:01 -0400

    Moving POST logic out of view

--- js/controllers/questions_controller.js
+++ js/controllers/questions_controller.js
@@ -8,20 +8,9 @@ Lickr.QuestionController = Ember.ObjectController.extend({
     actions: {
         test: function() {
             var current = this.get('model').get('id');
-            current = parseInt(current,10) + 1,
-            array = this.get('selectedImages');
-            array = JSON.stringify(array);
+            current = parseInt(current,10) + 1;
 
             if(current > this.get('numModels')){
-                // TODO: post to Flask to do image processing
-                $.ajax({
-                    type: 'POST',
-                    url: 'http://127.0.0.1:8000/process_imgs',
-                    data: {'imgs': array}
-                }).success(function(data){
-                    console.log(data);
-                });
-
                 this.transitionToRoute('results');
             } else this.transitionToRoute('question',current);
         },
