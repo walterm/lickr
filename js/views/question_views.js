b2e517657bad0f6f9ca3ca409fca10bdb699266c
Walter Menendez <walterm@mit.edu>
2015-04-06 01:21:54 -0400

    Logic + CSS needed

--- js/views/question_views.js
+++ js/views/question_views.js
@@ -25,17 +25,25 @@ Lickr.ResultsView = Ember.View.extend({
             url: 'http://127.0.0.1:8000/process_imgs',
             data: {'imgs': array}
         }).success(function(data){
-            data = JSON.parse(data);
             var colorDivs = [];
+            data = JSON.parse(data);
+            
             $("#results_header > h1").empty();
-            $("#results_header > h1").html("Your color palette is");
-            $("#results_body").empty();
-            _.each(data.colors, function (color){
-                console.log(color);
-                var color_div = $("<div class='color'></div>");
+            $("#results_header > h1").html("Your color palette is...");
+            $("#results_body_top").empty();
+            for(var i = 0; i < data.colors.length; i++){
+                var color = data.colors[i],
+                    color_div = $("<div class='color'></div>"),
+                    code_div = $("<p class='code'></p>");
                 $(color_div).css("background-color", color);
-                $("#results_body").append(color_div);
-            });
+                $(code_div).html(color);
+                $(color_div).append(code_div);
+                if(i < 2) {;
+                    $("#results_body_top").append(color_div);
+                } else {
+                    $("#results_body_bottom").append(color_div);
+                }
+            }
         });
     }
 });
