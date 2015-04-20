7d3e0008f182a139d695bf91da12f7bb89459597
Walter Menendez <walterm@mit.edu>
2015-04-20 14:16:08 -0400

    Now scraping flickr, and preventing duplicate downloads

--- process_images.py
+++ process_images.py
@@ -1,8 +1,29 @@
+from collections import defaultdict
 from urlparse import urlparse
 import urllib
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+
+
+images = defaultdict(bool)
+
 
 def get_imgur_pic(parsed_url):
     pass
+
+
+def get_flickr_pic(link, counter):
+    global images  # sadface
+    request = urlopen(link)
+    soup = BeautifulSoup(request.read())
+    imgs = soup.find_all('img')
+    for img in imgs:
+        if 'c1.staticflickr.com' in img['src'] and not images[img['src']]:
+            images[img['src']] = True
+            urllib.urlretrieve(img['src'], './imgs/'+str(counter) + '.jpg')
+            counter += 1
+    return counter
+
 
 with open('reddit_pics.txt', 'r') as f:
     links = f.readlines()
