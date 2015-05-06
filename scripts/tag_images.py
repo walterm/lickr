efe53f66e5714df29bcab2f704f3c01c7ca0d6de
Walter Menendez <walterm@mit.edu>
2015-05-06 03:07:28 -0400

    CONVERSION SADFACE

--- scripts/tag_images.py
+++ scripts/tag_images.py
@@ -4,6 +4,7 @@ import scipy.misc
 import scipy.cluster
 import numpy as np
 from numpy.linalg import norm
+import struct
 
 HEX_RED = 'ff0000'
 HEX_ORANGE = 'ffa500'
@@ -12,6 +13,42 @@ HEX_GREEN = '008000'
 HEX_BLUE = '0000ff'
 HEX_PURPLE = '800080'
 TARGETS = [HEX_RED, HEX_ORANGE, HEX_YELLOW, HEX_GREEN, HEX_BLUE, HEX_PURPLE]
+
+
+def compute_rgb(hex_code):
+    return struct.unpack('BBB', hex_code.decode('hex'))
+
+
+def palette_cluster(hex_codes):
+    rgb_colors = [compute_rgb(code) for code in hex_codes]
+    color_matrix = np.zeros((len(rgb_colors), 3))
+    for i in range(len(rgb_colors)):
+        color_matrix[i, :] = rgb_colors[i]
+    centers, dist = scipy.cluster.vq.kmeans(color_matrix, 4)
+    centers = centers.astype(int)
+    return [''.join(chr(c) for c in center).encode('hex') for center in centers]
+
+
+def random_palette(hex_codes):
+    def partition(lst, n): 
+        division = len(lst) / float(n) 
+        return [ lst[int(round(division * i)): int(round(division * (i + 1)))] for i in xrange(n) ]
+
+    def rgb_to_hex(rgb):
+        return '%02x%02x%02x' % rgb
+
+    from random import shuffle
+
+    rgb_colors = [compute_rgb(code) for code in hex_codes]
+    shuffle(rgb_colors)
+    splits = partition(rgb_colors, 4)
+    averages = []
+    for split in splits:
+        avg = [sum(c) / len(c) for c in zip(*split)]
+        avg = tuple(avg)
+        averages.append(avg)
+    averages = [rgb_to_hex(avg) for avg in averages]
+    return averages
 
 
 def compute_colors(image_path):
@@ -38,13 +75,6 @@ def compute_closest(hex_code):
         a = np.array(c1)
         b = np.array(c2)
         return norm(a-b)
-
-    def compute_rgb(hex_code):
-        return (
-            int(hex_code[0:2].encode('hex')),
-            int(hex_code[2:4].encode('hex')),
-            int(hex_code[4:].encode('hex'))
-        )
 
     rgb = compute_rgb(hex_code)
     c = compute_rgb(TARGETS[0])
@@ -94,16 +124,8 @@ def compute_conf_img_similarity(conf_dict, img):
     similarity = 0.
     for i in range(len(codes)):
         try:
-            code = (
-                int(codes[i][0:2].encode('hex')),
-                int(codes[i][2:4].encode('hex')),
-                int(codes[i][4:].encode('hex'))
-            )
-            color = (
-                int(img['top_colors'][i][0:2].encode('hex')),
-                int(img['top_colors'][i][2:4].encode('hex')),
-                int(img['top_colors'][i][4:].encode('hex'))
-            )
+            code = compute_rgb(codes[i])
+            color = compute_rgb(img['top_colors'][i])
             similarity += (compute_norm(code, color)) * conf_dict[codes[i]]
         except:
             continue
